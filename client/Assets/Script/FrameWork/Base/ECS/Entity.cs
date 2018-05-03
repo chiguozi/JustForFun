e983bfa57d10e76cf8a93a35b1d545d7105daaf6
chiguozi <qdlzr456@163.com>
2018-05-03 23:08:39 +0800

    ecs

---
+++ client/Assets/Script/FrameWork/Base/ECS/Entity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace IDK.Runtime
+{
+    public class Entity : IEntity
+    {
+        public uint EntityId
+        {
+            get { return 0; }
+        }
+        public void AddComponent<T>(T com) where T : IComponent
+        { }
+
+        public void RemoveComponent<T>(T com) where T : IComponent
+        { }
+
+        public bool HasComponent<T>() where T:IComponent
+        {
+            return true;
+        }
+
+        public void Dispose()
+        { }
+
+        protected virtual void OnDispose()
+        { }
+    }
+
+}
---
+++ client/Assets/Script/FrameWork/Base/ECS/IECS.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace IDK.Runtime
+{
+    public interface ISystemManager
+    {
+        void RegistSystem(ISystem sys);
+    }
+
+    public interface IEntityManager
+    {
+        IEntity CreateEntity();
+        IEntity GetEntity(uint entityId);
+
+        void DelEntity(uint entityId);
+        void DelEntity(IEntity entity); 
+    }
+
+    public interface ISystem
+    {
+    }
+
+    public interface IComponent
+    {
+    }
+
+    public interface IEntity
+    {
+        uint EntityId { get; }
+
+        void AddComponent<T>(T com) where T : IComponent;
+        void RemoveComponent<T>(T com) where T: IComponent;
+
+
+        bool HasComponent<T>() where T : IComponent;
+
+        void Dispose();
+    }
+}
+
